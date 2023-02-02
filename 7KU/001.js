b13c1be98936d73e0aa4dad9dff14650d5c17234
Andrey Sery <andrey.72006@gmail.com>
2023-02-02 23:01:19 +0300

    002

--- 7KU/001.js
+++ 7KU/001.js
@@ -6,8 +6,8 @@ function covfefe(str) {
   return str.replaceAll('coverage', 'covfefe');
 }
 
-console.log(covfefe('coverage')); //'covfefe'
-console.log(covfefe('coverage coverage')); //'covfefe covfefe'
-console.log(covfefe('nothing')); //'nothing covfefe'
-console.log(covfefe('double space ')); //'double space  covfefe'
-console.log(covfefe('covfefe')); //'covfefe covfefe'
+// console.log(covfefe('coverage')); //'covfefe'
+// console.log(covfefe('coverage coverage')); //'covfefe covfefe'
+// console.log(covfefe('nothing')); //'nothing covfefe'
+// console.log(covfefe('double space ')); //'double space  covfefe'
+// console.log(covfefe('covfefe')); //'covfefe covfefe'
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 'use strict';
 
 import './7KU/001.js';
+import './7KU/002.js';

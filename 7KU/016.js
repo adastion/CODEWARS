80d57319a856799e6ba467541cd7c08ae0cf5517
Andrey Sery <andrey.72006@gmail.com>
2023-02-14 20:46:22 +0300

    016

---
+++ 7KU/016.js
@@ -0,0 +1,25 @@
+function solution(string) {
+  let res = string.split('');
+  let arr = [];
+  res.map((item, i) => {
+    if ((item === item.toUpperCase())) {
+      return (res[i] = ' ' + item);
+    } else {
+      return item;
+    }
+
+  });
+
+  return res.join('');
+}
+
+
+
+// regular exspression
+
+// function solution(string) {
+//   return string.replace(/([A-Z])/g, ' $1');
+// }
+
+console.log(solution('camelCasing')); // 'camel Casing', 'Unexpected result');
+console.log(solution('camelCasingTest')); // 'camel Casing Test', 'Unexpected result');
